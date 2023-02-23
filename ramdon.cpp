c9a83ad2e53634c41978796d04809c836d130d9a
Yash Sharma <122903972+Gemini-YashSharma@users.noreply.github.com>
2023-02-23 13:04:57 +0530

    Create ramdon.cpp

---
+++ ramdon.cpp
@@ -0,0 +1,42 @@
+bool isStringValid(string inputstr) {
+    
+    if(inputstr.size()<3)
+        return false;
+    
+    unordered_set<char> s;
+    unordered_set<string> ss;
+    
+        ss.insert("gem");
+        ss.insert("gini");
+        ss.insert("mini");
+        s.insert('g');
+        s.insert('e');
+        s.insert('m');
+        s.insert('n');
+        s.insert('i');
+    
+    int n=inputstr.size();
+    for(int i=0; i<n; i++)
+    {    
+        if(s.find ( inputstr[i] ) == s.end())
+            return false;
+    }
+    
+    string t="";
+    
+    for(int i=0; i<inputstr.size();i++){
+        if(t.size()>4)
+            return false;
+        
+        if(ss.find(t)==ss.end())
+            t+=inputstr[i];
+        
+        if(ss.find(t)!=ss.end())
+            t="";
+    }
+    
+    if(t!="")
+        return false;
+    
+    return true;
+}
